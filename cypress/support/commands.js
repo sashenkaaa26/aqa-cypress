a4c5ad44fcee1d6245471ffeeec8186101c2534d
sashenkaaa26 <alexandra.mladinova@gmail.com>
2025-04-06 16:29:38 +0300

    Completed HW from lesson 19

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,4 +1,42 @@
 // ***********************************************
+Cypress.Commands.add('login', (email, password) => {
+  cy.visit('https://qauto.forstudy.space/', {
+    auth: {
+      username: 'guest',
+      password: 'welcome2qauto',
+    },
+  });
+
+  cy.get('button.btn.btn-outline-white.header_signin').click();
+  cy.get('#signinEmail').type(email);
+  cy.get('#signinPassword').type(password, { sensitive: true });
+
+  cy.get('div.modal-footer')
+    .contains('Login')
+    .should('not.be.disabled')
+    .click();
+
+    cy.get('button#userNavDropdown', { timeout: 10000 }).should('be.visible');
+});
+
+Cypress.Commands.overwrite('type', (originalFn, element, text, options = {}) => {
+  if (options.sensitive) {
+    options.log = false;
+    Cypress.log({
+      $el: element,
+      name: 'type',
+      message: '*'.repeat(text.length),
+    });
+  }
+
+  return originalFn(element, text, options);
+});
+
+
+
+
+
+
 // This example commands.js shows you how to
 // create various custom commands and overwrite
 // existing commands.
